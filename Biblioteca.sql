06d470f28daaba30b2fc4f8959d8d99253baab04
santicr3prueba <santicr3@gmail.com>
2022-12-22 20:57:22 +0100

    Treball

--- Biblioteca.sql
+++ Biblioteca.sql
@@ -5,7 +5,7 @@ USE biblioteca;
 
 #Un socio puede tener un libro
 CREATE TABLE Socio(
-	dni INT PRIMARY KEY,
+	dni VARCHAR(9) PRIMARY KEY,
     nom VARCHAR(30),
 	direccio VARCHAR(30),
     tiene INT
@@ -15,10 +15,8 @@ CREATE TABLE Socio(
 CREATE TABLE Biblioteca(
     nom VARCHAR(30) PRIMARY KEY,
     direccio VARCHAR(30),
-    es_socio INT,
-    esta INT,
-    FOREIGN KEY (es_socio) REFERENCES Socio(dni),
-    FOREIGN KEY (esta) REFERENCES Esta(libro)
+    es_socio VARCHAR(9),
+    esta INT
 );
 
 # Muchos libros pueden estar en muchas bibliotecas
@@ -26,11 +24,8 @@ CREATE TABLE Libro(
 	id INT PRIMARY KEY,
     titulo VARCHAR(50),
     autor VARCHAR(30),
-    esta VARCHAR(30),
-    FOREIGN KEY (esta) REFERENCES Esta(biblioteca)
+    esta VARCHAR(30)
 );
-
-ALTER TABLE Socio ADD CONSTRAINT tiene FOREIGN KEY (tiene) REFERENCES Libro(id) ON DELETE CASCADE ON UPDATE CASCADE;
 
 
 CREATE TABLE Esta(
@@ -39,3 +34,8 @@ CREATE TABLE Esta(
     FOREIGN KEY (biblioteca) REFERENCES Biblioteca(nom),
     FOREIGN KEY (libro) REFERENCES Libro(id)
 );
+
+ALTER TABLE Socio ADD CONSTRAINT FK_tiene FOREIGN KEY (tiene) REFERENCES Libro(id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE Biblioteca ADD CONSTRAINT FK_es_socio FOREIGN KEY (es_socio) REFERENCES Socio(dni) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE Biblioteca ADD CONSTRAINT FK_esta_b FOREIGN KEY (esta) REFERENCES Esta(libro) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE Libro ADD CONSTRAINT FK_esta_l FOREIGN KEY (esta) REFERENCES Esta(biblioteca) ON DELETE CASCADE ON UPDATE CASCADE;
